9ff8e6a05d60b6a8d86e0b0fbf5674907facda85
Roger Rosa <chozoace@gmail.com>
2018-08-18 19:13:13 -0400

    Mobile integration intial

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
-import HomePage from './HomePage';
+import HomepageLayout from './HomePage';
 import Footer from './Footer';
 import NavBar from './HeaderComponent/NavBar';
 
@@ -11,7 +11,8 @@ class App extends Component {
             <Router>
                 <div>
                     {/*<NavBar />*/}
-                    <Route name = "home" exact path="/" component={HomePage} />
+                    {/*<Route name = "home" exact path="/" component={HomepageLayout} />*/}
+                    <HomepageLayout/>
                     <Footer />
                 </div>
             </Router>
--- react-client/src/Components/HomePage.jsx
+++ react-client/src/Components/HomePage.jsx
@@ -18,7 +18,41 @@ import {
     Visibility,
   } from 'semantic-ui-react'
 
-class HomePage extends Component {
+const HomepageHeading = ({ mobile }) => (
+    <Container text>
+    <Header
+      as='h1'
+      content='Imagine-a-Company'
+      inverted
+      style={{
+        fontSize: mobile ? '2em' : '4em',
+        fontWeight: 'normal',
+        marginBottom: 0,
+        marginTop: mobile ? '1.5em' : '3em',
+      }}
+    />
+    <Header
+      as='h2'
+      content='Do whatever you want when you want to.'
+      inverted
+      style={{
+        fontSize: mobile ? '1.5em' : '1.7em',
+        fontWeight: 'normal',
+        marginTop: mobile ? '0.5em' : '1.5em',
+      }}
+    />
+    <Button primary size='huge'>
+      Get Started
+      <Icon name='right arrow' />
+    </Button>
+  </Container>
+)
+
+HomepageHeading.propTypes = {
+    mobile: PropTypes.bool,
+}
+
+class DesktopContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {};
@@ -67,7 +101,7 @@ class HomePage extends Component {
                                 </Menu.Item>
                             </Container>
                         </Menu>
-                        {/*homepage heading*/}
+                        <HomepageHeading />
                     </Segment>
                 </Visibility>
                 {children}
@@ -76,4 +110,98 @@ class HomePage extends Component {
     }
 }
 
-export default HomePage;
+DesktopContainer.propTypes = {
+    children: PropTypes.node,
+}
+
+class MobileContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    handlePusherClick() {
+        const { sidebarOpened } = this.state
+
+        if(sidebarOpened) this.setState({sidebarOpened: false})
+    }
+
+    handleToggle() {
+        this.setState({sidebarOpened: !this.state.sidebarOpened})
+    } 
+
+    render() {
+        const { children } = this.props
+        const { sidebarOpened } = this.state
+
+        return (
+            <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
+                <Sidebar.Pushable>
+                    <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened}>
+                        <Menu.Item as='a' active>
+                            Home
+                        </Menu.Item>
+                        <Menu.Item as='a'>About Us</Menu.Item>
+                        <Menu.Item as='a'>Dance</Menu.Item>
+                        <Menu.Item as='a'>Contact</Menu.Item>
+                        <Menu.Item as='a'>Log In</Menu.Item>
+                        <Menu.Item as='a'>Sign Up</Menu.Item>
+                    </Sidebar>
+                    <Sidebar.Pusher
+                        dimmed = {sidebarOpened}
+                        onClick={() => handlePusherClick}
+                        style={{minHeight: '100vh'}}
+                    >
+                        <Segment
+                            inverted
+                            textAlign='center'
+                            style={{minHeight: 350, padding: '1em 0em'}}
+                            vertical
+                        >
+                            <Container>
+                                <Menu inverted pointing secondary size='large'>
+                                    <Menu.Item onClick={() => handleToggle}>
+                                        <Icon name='sidebar'/>
+                                    </Menu.Item>
+                                    <Menu.Item position='right'>
+                                        <Button as='a' inverted>
+                                            Log in
+                                        </Button>
+                                        <Button as='a' inverted style={{marginLeft:'0.5em'}}>
+                                            Sign up
+                                        </Button>
+                                    </Menu.Item>
+                                </Menu>
+                            </Container>  
+                            <HomepageHeading mobile/> 
+                        </Segment>
+                        {children}         
+                    </Sidebar.Pusher>
+                </Sidebar.Pushable>
+            </Responsive>
+        )
+    }
+
+}
+
+MobileContainer.propTypes = {
+    children: PropTypes.node,
+}
+
+const ResponsiveContainer = ({ children }) => (
+    <div>
+      <DesktopContainer>{children}</DesktopContainer>
+      <MobileContainer>{children}</MobileContainer>
+    </div>
+)
+
+ResponsiveContainer.propTypes = {
+    children: PropTypes.node,
+}
+
+const HomepageLayout = () => (
+    <ResponsiveContainer>
+    </ResponsiveContainer>
+)
+
+export default HomepageLayout;
